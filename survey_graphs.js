8c0ee6c369a1e11a1459a9da1816d6260b91366f
Kavi Mehta <kavi.mehta2@gmail.com>
2016-12-18 20:30:21 -0800

    Wrote javascript functions

--- survey_graphs.js
+++ survey_graphs.js
@@ -1,5 +1,4 @@
-function getUserData() {
-	var username = document.getElementById('Username').value;
+function getUserData(username) {
 	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?ts=58572ebe#gid=97524261');
 	query.setQuery('select * where B = ' + username);
 	query.send(handleQueryResponse);
@@ -10,6 +9,12 @@ function handleQueryResponse(queryResponse) {
 	if (userTable == null) {
 		alert('The username you entered does not match any entries.')
 	} else {
-
+		var userView = new google.visualization.DataView(userTable).setColumns([0, 23])
+		var obj = new Object();
+		obj.chartType = 'LineChart';
+		obj.containerId = document.createElement("CANVAS").id;
+		obj.dataTable = userTable;
+		obj.view = userView;
+		google.visualization.drawChart(obj);
 	}
 }
