3259518de9a70b67a0f565011da6c6d4ea335e58
Kavi Mehta <kavi.mehta2@gmail.com>
2016-12-18 22:35:28 -0800

    Added lines to load chart API and fixed JS functions. Query times out

--- survey_graphs.js
+++ survey_graphs.js
@@ -1,20 +1,26 @@
-function getUserData(username) {
-	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?ts=58572ebe#gid=97524261');
-	query.setQuery('select * where B = ' + username);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+	username = document.getElementById('username').value;
+	if (username == "") {
+		return;
+	}
+	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/pubhtml');
+	query.setQuery("SELECT A, X WHERE B = " + username + " ORDER BY A");
 	query.send(handleQueryResponse);
 }
 
-function handleQueryResponse(queryResponse) {
-	var userTable = queryResponse.getDataTable();
+function handleQueryResponse(response) {
+	if (response.isError()) {
+		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return;
+	}
+	var userTable = response.getDataTable();
 	if (userTable == null) {
 		alert('The username you entered does not match any entries.')
 	} else {
-		var userView = new google.visualization.DataView(userTable).setColumns([0, 23])
-		var obj = new Object();
-		obj.chartType = 'LineChart';
-		obj.containerId = document.createElement("CANVAS").id;
-		obj.dataTable = userTable;
-		obj.view = userView;
-		google.visualization.drawChart(obj);
+		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		chart.draw(data, null);
 	}
 }
