ee324c9b5895ef9b080ecc0d5a91033e8d71fcbc
Kavi Mehta <kavi.mehta2@gmail.com>
2016-12-18 23:23:49 -0800

    Querying and Charting works

--- survey_graphs.js
+++ survey_graphs.js
@@ -1,4 +1,4 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart', 'line']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
@@ -6,8 +6,8 @@ function drawChart() {
 	if (username == "") {
 		return;
 	}
-	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/pubhtml');
-	query.setQuery("SELECT A, X WHERE B = " + username + " ORDER BY A");
+	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?usp=sharing/gviz/tq');
+	query.setQuery("SELECT A, X WHERE B = '" + username + "' ORDER BY A");
 	query.send(handleQueryResponse);
 }
 
@@ -17,10 +17,20 @@ function handleQueryResponse(response) {
 		return;
 	}
 	var userTable = response.getDataTable();
-	if (userTable == null) {
+	if (userTable.getNumberOfRows() == 0) {
 		alert('The username you entered does not match any entries.')
 	} else {
 		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-		chart.draw(data, null);
+		var options = {
+			pointSize : 3,
+			vAxis: {
+	            viewWindowMode:'explicit',
+	            viewWindow: {
+	              max:100,
+	              min:0
+            	}
+            }
+		}
+		chart.draw(userTable, options);
 	}
 }
