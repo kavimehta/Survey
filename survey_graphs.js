d7ab47e193c226765deba18fd8bdadaeec62e7eb
Kavi Mehta <kavi.mehta2@gmail.com>
2016-12-19 11:56:52 -0800

    Added individual line graph. Added link back to homepage. Improved link stling. Survey link opens new tab.

--- survey_graphs.js
+++ survey_graphs.js
@@ -6,12 +6,16 @@ function drawChart() {
 	if (username == "") {
 		return;
 	}
-	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?usp=sharing/gviz/tq');
-	query.setQuery("SELECT A, X WHERE B = '" + username + "' ORDER BY A");
-	query.send(handleQueryResponse);
+	var query1 = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?usp=sharing/gviz/tq');
+	query1.setQuery("SELECT A, X WHERE B = '" + username + "' ORDER BY A");
+	query1.send(handleQueryResponse1);
+
+	var query2 = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1pkfeP8RcJEE0nha_duBzI6L7Yt3EacfqOK3H0RecjxE/edit?usp=sharing/gviz/tq');
+	query2.setQuery("SELECT A, N, O, P, Q, R, S, T, U, V, W WHERE B = '" + username + "' ORDER BY A");
+	query2.send(handleQueryResponse2);
 }
 
-function handleQueryResponse(response) {
+function handleQueryResponse1(response) {
 	if (response.isError()) {
 		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
 		return;
@@ -20,7 +24,7 @@ function handleQueryResponse(response) {
 	if (userTable.getNumberOfRows() == 0) {
 		alert('The username you entered does not match any entries.')
 	} else {
-		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		var chart = new google.visualization.LineChart(document.getElementById('agg_chart_div'));
 		var options = {
 			pointSize : 3,
 			vAxis: {
@@ -31,6 +35,21 @@ function handleQueryResponse(response) {
             	}
             }
 		}
+		chart.draw(userTable, options);
+	}
+}
+
+function handleQueryResponse2(response) {
+	if (response.isError()) {
+		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return;
+	}
+	var userTable = response.getDataTable();
+	if (userTable.getNumberOfRows() == 0) {
+		alert('The username you entered does not match any entries.')
+	} else {
+		var chart = new google.charts.Line(document.getElementById('individual_chart_div'));
+		var options = null;
 		chart.draw(userTable, options);
 	}
 }
